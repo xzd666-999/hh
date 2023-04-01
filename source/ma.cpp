e2be20dd2dd378cc153d6bf5f1087254edbf2371
xiaozd <916691771@qq.com>
2023-04-01 15:07:05 +0800

    "第五步，新增一个ma.cpp"

---
+++ source/ma.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+const int N = 100010;
+int main()
+{
+    int a[N]={0};
+    for(auto & v : a)
+    {
+        ++v;
+        cout << v << ' ';
+    }
+    return 0;
+}
